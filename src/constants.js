2eec3a597ffea15e74c1aa2fd3e7bf14d2e3fd33
Ismail Majid <ismail.majid@mail.utoronto.ca>
2025-06-23 13:27:07 -0400

    This is my first commit for my Kaboom.js game project.

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my laptop. I work mostly in R these days, since I work as a research assistant at Jackson Labs @ UofT.
+  I also like Python and Java. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/majidism006" target="_blank">Github</a>!`,
+  "cs-degree": `This is my High School diploma. I hung it on the wall because I'm proud of it. I received a regional distinction for being among the highest scorers in the UAE.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch Tiktoks (yes -- the brainrot is bad).`,
+  tv: `That's my TV. I've been watching alot of anime like spyxfamily and jjk!`,
+  bed: `Ahhhhh my bed, I could spend all day here.`,
+  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1J3Uu12JABCTjR-DS9u2yg6geVkZ719ry/view?usp=sharing" target="_blank">Check it out?</a>
+  Contact me at ismail.majid@mail.utoronto.ca if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  library: `Reading and writing have both always been hobbies of mine since I was a kid. I also love painting and drawing (fun fact: I took art as a subject in school!)`,
+  exit: `If you want to exit Ismail's portfolio, just close the tab.`,
+};

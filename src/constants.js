340de287ad0397f586cba3ea2774c9908403f753
Ismail Majid <ismail.majid@mail.utoronto.ca>
2025-06-26 15:18:04 -0400

    removing projects pop up

--- src/constants.js
+++ src/constants.js
@@ -10,8 +10,6 @@ export const dialogueData = {
   frog: `FROGGGGGIIEEEEEEEEEEEE...`,
   resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1J3Uu12JABCTjR-DS9u2yg6geVkZ719ry/view?usp=sharing" target="_blank">Check it out?</a>
   Contact me at ismail.majid@mail.utoronto.ca if you have any interesting job opportunities! Here is my <a href="https://github.com/majidism006" target="_blank">Github</a>!`,
-  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS.`,
   library: `Reading and writing have both always been hobbies of mine since I was a kid. I also love painting and drawing (fun fact: I took art as a subject in school!)`,
   kitchen: `I LOVEEE COOKING!!! I can make killer jollof rice and chicken karahi.`,
 };

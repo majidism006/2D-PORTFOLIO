e98762f67bea1a16ef3e108780d3c139d7c6f20a
Ismail Majid <ismail.majid@mail.utoronto.ca>
2025-07-20 18:08:04 -0400

    updated resume

--- src/constants.js
+++ src/constants.js
@@ -6,7 +6,7 @@ export const dialogueData = {
     Here is my <a href="https://github.com/majidism006" target="_blank">Github</a>!`,
   "sofa-table": `That's my sofa. I like to relax here and watch Tiktoks (yes -- the brainrot is bad).`,
   frog: `FROGGGGGIIEEEEEEEEEEEE...`,
-  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1IJsDZthw-flmw8PF8WCEmy0s1eTCQp64/view?usp=sharing">Check it out?</a>
+  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1KKtmjV4lU7uf-zaxaiTEp9UV5EAevv-G/view?usp=sharing">Check it out?</a>
   Contact me at ismail.majid@mail.utoronto.ca if you have any interesting job opportunities! Here is my <a href="https://github.com/majidism006" target="_blank">Github</a>!`,
   library: `Reading and writing have both always been hobbies of mine since I was a kid. I also love painting and drawing (fun fact: I took art as a subject in school!)`,
   kitchen: `I LOVEEE COOKING!!! I can make killer jollof rice and chicken karahi.`,
